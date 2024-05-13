c3ea4943d3cf71dcff91fd63588ef349a0e6292f
unknown <yugansethu@gmail.com>
2024-05-13 20:10:34 +0530

    added files cicd

---
+++ src/main/java/com/gcpdevops/kloud4devops/Kloud4devopsApplication.java
@@ -0,0 +1,13 @@
+package com.gcpdevops.kloud4devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kloud4devopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kloud4devopsApplication.class, args);
+	}
+
+}
